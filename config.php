7d5099116c6782f0dfed83ee09f1d88f1f7ab9ba
Emac <shen.bin.1983@gmail.com>
2015-12-20 01:48:18 +0800

    add config.php --continue

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$blog_email = "shen.bin.1983@gmail.com";
+$blog_twitter = "dropplets";
+$blog_url = "http://emacoo.sinaapp.com/";
+$blog_title = "Welcome to Dropplets";
+$meta_description = "";
+$intro_title = "Welcome to Dropplets";
+$intro_text = "In a flooded selection of overly complex solutions, Dropplets has been created in order to deliver a much needed alternative. There is something to be said about true simplicity in the design, development and management of a blog. By eliminating all of the unnecessary elements found in typical solutions, Dropplets can focus on pure design, typography and usability. Welcome to an easier way to blog.";
+$password = '$2a$08$5FydCPAHcmkQOqROheFrVuaLyshxXU37/ODzvp/woTLchELwsEGKe';
+$header_inject = "";
+$footer_inject = "";
+$template = "simple";
